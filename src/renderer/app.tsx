bbe5b4ce1e579a6a4363dfccf954a817d06e4ce1
Guillaume Raille <guillaume.raille@icloud.com>
2024-12-23 22:14:15 +0100

    structure repo

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,4 +1,4 @@
 import { createRoot } from 'react-dom/client';
 
-const root = createRoot(document.body);
+const root = createRoot(document.getElementById('root'));
 root.render(<h2>Hello from React!</h2>);
