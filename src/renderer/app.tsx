8bcb86c5fda200c21b299c293a59c395d55d39f5
Guillaume Raille <guillaume.raille@icloud.com>
2024-12-27 15:55:32 +0100

    notifications now added to the instructions component

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -2,7 +2,6 @@ import { createRoot } from "react-dom/client";
 
 import { Header } from "./components/Header";
 import { Instructions } from "./components/Instructions";
-import { Test } from "./components/Test";
 import { Toaster } from "./components/ui/toaster";
 
 const root = createRoot(document.getElementById("root"));
@@ -10,7 +9,6 @@ root.render(
   <>
     <Header />
     <Instructions />
-    <Test />
     <Toaster />
   </>
 );
